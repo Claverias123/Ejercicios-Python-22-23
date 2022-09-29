9dc1aacd5d1a16cc2219d3b2becf7f1898afeadb
Claverias123 <al_amartinezc@iessierradelasvillas.org>
2022-09-29 10:03:07 +0200

    Comentarios, entrada, salida de datos y tipos de datos en python

---
+++ Hola mundo.py
@@ -0,0 +1,22 @@
+nombre="Álvaro"
+edad=17
+mayorOmenorEdad= False
+
+
+
+
+
+
+#Entrada de salida de datos
+#Salida con print
+
+print("Buenos días",nombre, "tu tienes", edad, "años")
+
+
+#Entrada de datos -- input()
+nombre = input ("Dime tu nombre:\n")
+edad = input ("Dime tu edad:\n")
+print("Buenos días",nombre, ",tu tienes", edad, "años")
+
+
+
