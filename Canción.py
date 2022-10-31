2ad2ef03f2f102dcac8549adb58f82b4d08fc7e9
Claverias123 <al_amartinezc@iessierradelasvillas.org>
2022-10-31 10:27:47 +0100

    musica

--- Canción.py
+++ Canción.py
@@ -11,16 +11,9 @@ def descargaCancion(url:str):
     cancion=youtube.streams.get_audio_only()
     cancion.download()
 
-descargaCancion("https://www.youtube.com/shorts/vCiukaljhO4")
+descargaCancion("https://www.youtube.com/watch?v=pzDWAWJ99z0")
 
 
-def descargarLista(url:str):
-    playlist=Playlist(url)
-    for cancion in playlist.videos:
-        print("Descargando cancion:", cancion.title)
-        cancion.streams.get_audio_only().download("cancion/")
-        print("*************************\n")
-url="https://www.youtube.com/watch?v=kgcm0_wxHFQ&list=PLVZtocpycmEUbKbqDt1bnnA67njRJHZll"
-descargarLista(url)
+
 
 
